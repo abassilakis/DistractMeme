900e39169cbbed243d57269a77746061c5394e77
estebanreyl <erey@stanford.edu>
2017-02-18 13:49:19 -0800

    Initial commit

---
+++ app/src/main/java/com/distractmeme/distractmeme/SwipeMeme.java
@@ -0,0 +1,13 @@
+package com.distractmeme.distractmeme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwipeMeme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_meme);
+    }
+}
